e80540df362c6e7c17c7fe16412b6ec942c24893
SiriuXProtocoL <bebinkrajusalkara@gmail.com>
2021-02-07 10:20:17 +0530

    API token added

--- quotes/views.py
+++ quotes/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
+#import from the internet
+import requests
+#parse it
+import json
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_183750c8ece6430f84f747bfb19be397")
+    
+    try:
+        api = json.loads(api_request.content)
+
+    #if there api does not return anything as json
+    except Exception as e:
+        api = "Error..."
+
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
-    return render(request, 'about.html', {})
+
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_183750c8ece6430f84f747bfb19be397")
+    
+    try:
+        api = json.loads(api_request.content)
+
+    #if there api does not return anything as json
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'about.html', {'api': api})
